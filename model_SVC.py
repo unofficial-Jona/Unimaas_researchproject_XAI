7974698a44cbe48d75b8904288c6db1359a3dd6e
unofficial-Jona <79550985+unofficial-Jona@users.noreply.github.com>
2021-04-12 14:46:57 +0200

    created best_kernel function

--- model_SVC.py
+++ model_SVC.py
@@ -11,8 +11,18 @@ X_train = train.drop(["final_result"], axis = 1)
 y_test = test["final_result"].values
 X_test = test.drop(["final_result"], axis = 1)
 
-for i in ["linear", "poly", "rbf", "sigmoid"]:  # find best kernel --> linear, poly and rbf are about equal for given data Set
-    model = SVC(kernel=i)
-    model.fit(X_train, y_train)
-    score = model.score(X_test,y_test)
-    print(i, score)
+def best_kernel():
+    best_i = None
+    best_score = 0
+    for i in ["linear", "poly", "rbf", "sigmoid"]:  # find best kernel --> linear, poly and rbf are about equal for given data Set
+        model = SVC(kernel=i)
+        model.fit(X_train, y_train)
+        score = model.score(X_test,y_test)
+        # print(i, score)
+        if best_score < score:
+            best_score = score
+            best_i = i
+    return best_i, best_score
+
+kernel, score = best_kernel()
+print(f"best score is {score} with kernel {kernel}")
