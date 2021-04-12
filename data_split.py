f2c9a21653d114686f4eece78568150afaca02b8
Diego Penroz Valenzuela <penroz@unm.edu>
2021-04-12 19:40:20 +0200

    final result attribute changed to binary

--- data_split.py
+++ data_split.py
@@ -1,11 +1,14 @@
 import dataset
+import numpy as np
 from sklearn import model_selection
 
 
 df = dataset.load_data()
 train, test = model_selection.train_test_split(df, test_size=0.2)
 
-y_train = train["final_result"].values
+y_train_orig = train["final_result"].values
+y_train = np.where(y_train_orig == 'Distinction', 'Pass', y_train_orig)
 X_train = train.drop(["final_result"], axis = 1)
-y_test = test["final_result"].values
+y_test_orig = test["final_result"].values
+y_test = np.where(y_test_orig == 'Distinction', 'Pass', y_test_orig)
 X_test = test.drop(["final_result"], axis = 1)
--- model_SVC.py
+++ model_SVC.py
@@ -2,16 +2,13 @@ from data_split import X_test, X_train, y_train, y_test
 from sklearn.svm import SVC
 
 
-
-
-
 def run_SVC():
     best_i = None
     best_score = 0
-    for i in ["linear", "poly", "rbf", "sigmoid"]:  # find best kernel --> linear, poly and rbf are about equal for given data Set
+    for i in ["linear", "poly", "rbf", "sigmoid"]:  # find best kernel --> linear, poly and rbf are about equaSet
         model = SVC(kernel=i)
         model.fit(X_train, y_train)
-        score = model.score(X_test,y_test)
+        score = model.score(X_test, y_test)
         # print(i, score)
         if best_score < score:
             best_score = score
@@ -19,3 +16,10 @@ def run_SVC():
     return best_i, best_score
 
 # kernel, score = best_kernel()
+
+#Confusion Matrix - Use the whole data X, y without splitting into train, test
+#from sklearn.model_selection import cross_val_predict
+#from sklearn.metrics import confusion_matrix
+#Y_pred = cross_val_predict(model, X, y, cv=10)
+#print("and Confusion Matrix is")
+#print(confusion_matrix(y, Y_pred))
