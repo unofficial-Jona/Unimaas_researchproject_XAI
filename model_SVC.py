60f46758fb875cf6de0febd10a5f83d241e875be
Diego Penroz Valenzuela <penroz@unm.edu>
2021-04-12 19:59:05 +0200

    confusion matrix code added as comments

--- model_SVC.py
+++ model_SVC.py
@@ -5,22 +5,18 @@ from sklearn.svm import SVC
 def run_SVC():
     best_i = None
     best_score = 0
-    for i in ["linear", "poly", "rbf", "sigmoid"]:  # find best kernel --> linear, poly and rbf are about equaSet
+    for i in ["linear", "poly", "rbf", "sigmoid"]:  # find best kernel --> linear, poly and rbf are about equal.
         model = SVC(kernel=i)
         model.fit(X_train, y_train)
-<<<<<<< HEAD
-        score = model.score(X_test, y_test)
-        # print(i, score)
-=======
         score = model.score(X_test,y_test)
         print(i, score)
->>>>>>> 27a6d6892895d7c1a31e61d56e0ced370ff4208a
+
         if best_score < score:
             best_score = score
             best_i = i
     return best_i, best_score
 
-<<<<<<< HEAD
+
 # kernel, score = best_kernel()
 
 #Confusion Matrix - Use the whole data X, y without splitting into train, test...
@@ -29,6 +25,6 @@ def run_SVC():
 #Y_pred = cross_val_predict(model, X, y, cv=10)
 #print("and Confusion Matrix is")
 #print(confusion_matrix(y, Y_pred))
-=======
+
 kernel, score = run_SVC()
->>>>>>> 27a6d6892895d7c1a31e61d56e0ced370ff4208a
+
