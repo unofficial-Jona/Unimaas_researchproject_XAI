4572a7084f3d9ab6e3d0004595d52a451e6ed467
unofficial-Jona <79550985+unofficial-Jona@users.noreply.github.com>
2021-04-12 14:58:30 +0200

    new file for data_split + new model implemented

--- model_random_forest.py
+++ model_random_forest.py
@@ -2,8 +2,9 @@ from data_split import X_test, X_train, y_train, y_test
 
 from sklearn.ensemble import RandomForestClassifier
 
-clf = RandomForestClassifier()
-clf.fit(X_train,y_train)
 
-accuracy = clf.score(X_test,y_test)
-print(accuracy)
+def run_random_forest():
+    clf = RandomForestClassifier()
+    clf.fit(X_train,y_train)
+    accuracy = clf.score(X_test,y_test)
+    return accuracy
