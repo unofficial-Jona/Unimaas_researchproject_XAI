8bd70fc02895dd1e84da6fdc3c5a712963837283
unofficial-Jona <79550985+unofficial-Jona@users.noreply.github.com>
2021-04-26 16:11:59 +0200

    minor improvements to data_split.py

--- data_split.py
+++ data_split.py
@@ -1,22 +1,19 @@
 import dataset
 import numpy as np
+import pandas as pd
 from sklearn import model_selection
 from sklearn.base import BaseEstimator, TransformerMixin
 
+def update_df():
+    df = dataset.load_data()
+    df.to_csv("initialized_dataset", index=False)
 
-df = dataset.load_data()
-train, test = model_selection.train_test_split(df, test_size=0.2)
-
-y_train_orig = train["final_result"].values
-y_train = np.where(y_train_orig == 'Distinction', 'Pass', y_train_orig)
-X_train = train.drop(["final_result"], axis = 1)
-y_test_orig = test["final_result"].values
-y_test = np.where(y_test_orig == 'Distinction', 'Pass', y_test_orig)
-X_test = test.drop(["final_result"], axis = 1)
+# update_df()
+df = pd.read_csv('initialized_dataset')
 
 
 class PrepareDataset(BaseEstimator, TransformerMixin):
-    def __init__(self, prepare_keras = False ):
+    def __init__(self, prepare_keras=False):
         self.prepare_keras = prepare_keras
     def fit(self, X):
         return self
@@ -28,6 +25,7 @@ class PrepareDataset(BaseEstimator, TransformerMixin):
         y_test_orig = test["final_result"].values
         y_test = np.where(y_test_orig == 'Distinction', 'Pass', y_test_orig)
         X_test = test.drop(["final_result"], axis=1)
+
         if self.prepare_keras:
             y_train = np.where(y_train == 'Pass', 1, y_train)
             y_train = np.where(y_train == 'Fail', 0, y_train)
@@ -35,7 +33,11 @@ class PrepareDataset(BaseEstimator, TransformerMixin):
             y_test = np.where(y_test == "Pass", 1, y_test)
             y_test = np.where(y_test == "Fail", 0, y_test)
             y_test = np.asarray(y_test).astype('float32')
-
+            X_test.reindex()
+            X_train.reindex()
             return X_train, X_test, y_train, y_test
         else:
             return X_train, X_test, y_train, y_test
+
+data_prep = PrepareDataset()
+X_train, X_test, y_train, y_test = data_prep.transform(df)
