72d01f531966855646e8ee50fdc27e6be6d74088
unofficial-Jona <79550985+unofficial-Jona@users.noreply.github.com>
2021-04-26 16:12:35 +0200

    minor improvements to data_split.py

--- model_keras.py
+++ model_keras.py
@@ -8,16 +8,6 @@ data_prep = PrepareDataset(prepare_keras=True)
 X_train_ker, X_test_ker, y_train_ker, y_test_ker = data_prep.transform(df)
 
 
-'''
-y_train = np.where(y_train == "Pass", 1, y_train)
-y_train = np.where(y_train == "Fail", 0, y_train)
-y_train = np.asarray(y_train).astype('float32')
-
-y_test = np.where(y_test == "Pass", 1, y_test)
-y_test = np.where(y_test == "Fail", 0, y_test)
-y_test = np.asarray(y_test).astype('float32')
-
-'''
 model = Sequential()
 model.add(Dense(6, input_dim=6, activation="sigmoid"))
 model.add(Dense(8, activation="sigmoid"))
